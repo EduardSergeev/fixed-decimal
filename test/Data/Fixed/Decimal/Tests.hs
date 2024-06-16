59907427f5d6626e35aa18d2cc16e31e27bcc842
Eduard Sergeev <eduard.sergeev@gmail.com>
2024-06-16 12:47:19 +1200

    More tests for various precisions
    Extract generic test implementation into `Tests` module and run it for various `Decmial m s`'s `m`'s (including `Integer`

---
+++ test/Data/Fixed/Decimal/Tests.hs
@@ -0,0 +1,115 @@
+{-# LANGUAGE FlexibleContexts     #-}
+{-# LANGUAGE ScopedTypeVariables  #-}
+{-# LANGUAGE TypeOperators        #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module Data.Fixed.Decimal.Tests
+(
+    testFor
+) where
+
+import qualified Data.Decimal as DD
+import           Data.Fixed.Decimal
+import           Data.Scientific (Scientific(..), scientific)
+import           Data.Typeable (Typeable, typeOf)
+import           GHC.TypeLits (KnownNat, type (^))
+import           Test.Tasty (TestTree, testGroup)
+import           Test.Tasty.HUnit (testCase, (@?=))
+import           Test.Tasty.QuickCheck hiding (scale)
+
+
+testFor :: forall m s. (Integral m, Typeable m, KnownNat s, KnownNat (10 ^ s), Show m) => Decimal m s -> Decimal m s -> TestTree
+testFor dmin dmax =
+    testGroup (show $ typeOf dmin) [
+        testGroup "QuickCheck tests" [
+            testGroup "Roundtrip tests" [
+                testProperty "Scientific roundtrip"
+                    prop_sientificEq,
+                testProperty "Rational roundtrip"
+                    prop_rationalEq,
+                testProperty "Precision test"
+                    prop_precision  
+            ],
+    testGroup "Behaviour equivalence tests" [
+            testProperty "/ * test"
+                prop_divisionMultiplication,
+            testProperty "DD.Decimal equivalence in terms of addition"
+                prop_decimalPlusEq,
+            testProperty "show" $ do
+                d <- gen
+                pure $ show (fromDecimal @DD.Decimal d) === show d,
+            testProperty "abs" $ do
+                d <- gen
+                pure $ show (fromDecimal @DD.Decimal d) === show (abs d),
+            testProperty "signum" $ do
+                d <- gen
+                pure $ (show . signum @DD.Decimal . fromDecimal) d === (show . signum) d
+      ]
+    ],
+    testGroup "Specific Show cases" [
+        testCase "0" $
+            show (0 :: Decimal m s) @?= "0",
+        testCase "42" $
+            show (42 :: Decimal m s) @?= "42",
+        testCase "42.1" $
+            show (42.1 :: Decimal m s) @?= "42.1",
+        testCase "-42.01" $
+            show (-42.01 :: Decimal m s) @?= "-42.01",
+      testCase "-42.00042" $
+          show (-42.00042 :: Decimal m s) @?= "-42.00042"
+    ]
+  ]
+    where
+        gen :: Gen (Decimal m s)
+        gen = do
+            r <- Decimal . (* (10 ^ ((s `div` 2 + 1)))) . fromInteger <$> chooseInteger (minLim, maxLim)
+            pure r
+            where
+                s = scale dmax
+                p = floor @Double @Int . logBase 10 . fromIntegral . mantissa $ dmax
+                minLim = toInteger $ mantissa dmin `mod` (10 ^ (p `div` 3))        
+                maxLim = toInteger $ mantissa dmax `mod` (10 ^ (p `div` 3))        
+
+        prop_sientificEq = do
+            d <- gen
+            pure $ d === (fromScientific . toScientific) d
+
+        prop_rationalEq = do
+            d <- gen
+            pure $ d === (fromRational @(Decimal m s) . toRational $ d)
+
+        prop_divisionMultiplication = do
+            a <- gen
+            b <- gen
+            pure $ a /= 0 && b /= 0 ==> (a * b / a === b) .&&. (a * b / b === a)
+
+        prop_decimalPlusEq = do
+            dl <- gen
+            dr <- gen
+            let l = toRational dl
+                r = toRational dr
+                el = fromRational @DD.Decimal l
+                er = fromRational @DD.Decimal r
+            pure $ toRational (el + er) === toRational (dl + dr) 
+            where
+
+        prop_precision :: (Positive Int) -> (Positive Int) -> Property
+        prop_precision (Positive n) (Positive di) =
+            let d = fromIntegral @_ @Double di / 1000.0
+                de = fromIntegral @_ @(Decimal m s) di / 1000
+            in viaSum d /= (fromIntegral n) * d ==> viaSum de === (fromIntegral n) * de 
+            where
+                viaSum d = sum $ replicate n d
+
+        toScientific :: Decimal m s -> Scientific
+        toScientific d =
+            scientific (fromIntegral . mantissa $ d) (negate . scale $ d)
+
+        fromScientific :: Scientific -> Decimal m s
+        fromScientific s =
+            decimal (coefficient s) (base10Exponent s)
+
+        fromDecimal :: Fractional a => Decimal m s -> a
+        fromDecimal =
+            fromRational . toRational
--- test/Test.hs
+++ test/Test.hs
@@ -1,119 +1,19 @@
-{-# LANGUAGE TypeSynonymInstances #-}
+import Data.DoubleWord (Int128, Int256)
+import Data.Fixed.Decimal (Decimal)
+import Data.Fixed.Decimal.Tests (testFor)
+import Test.Tasty (TestTree, defaultMain, testGroup)
 
-import qualified Data.Decimal as DD
-import           Data.DoubleWord (Int256)
-import           Data.Fixed.Decimal hiding (Decimal)
-import qualified Data.Fixed.Decimal as D
-import           Data.Scientific (Scientific(..), scientific)
-import           Test.QuickCheck.Instances.Scientific ()
-import           Test.Tasty (TestTree, defaultMain, testGroup)
-import           Test.Tasty.HUnit (testCase, (@?=))
-import           Test.Tasty.QuickCheck hiding (scale)
-
-
-type FractionalPartPrecision = 15
-type Decimal = D.Decimal Int256 FractionalPartPrecision
 
 tests :: TestTree
 tests =
-  testGroup "Decimal tests" [
-    testGroup "QuickCheck tests" [
-      testGroup "Roundtrip tests" [
-        testProperty "Scientific roundtrip"
-          prop_sientificEq,
-        testProperty "Rational roundtrip"
-          prop_rationalEq,
-        testProperty "Precision test"
-          prop_precision  
-      ],
-      testGroup "Behaviour equivalence tests" [
-        testProperty "/ * test"
-          prop_divisionMultiplication,
-        testProperty "DD.Decimal equivalence in terms of addition"
-          prop_decimalPlusEq,
-        testProperty "show" $
-          \e -> show @DD.Decimal e === (show . fromRational @Decimal . toRational) e,
-        testProperty "abs" $
-          \e -> (show . abs @DD.Decimal) e === (show . abs . fromRational @Decimal . toRational) e,
-        testProperty "signum" $
-          \e -> (show . signum @DD.Decimal) e === (show . signum . fromRational @Decimal . toRational) e
-      ]
-    ],
-    testGroup "Specific Show cases" [
-      testCase "0" $
-          show (0 :: Decimal) @?= "0",
-      testCase "42" $
-          show (42 :: Decimal) @?= "42",
-      testCase "42.1" $
-          show (42.1 :: Decimal) @?= "42.1",
-      testCase "-42.0000001" $
-          show (-42.0000001 :: Decimal) @?= "-42.0000001",
-      testCase "0.00000042" $
-          show (0.00000042 :: Decimal) @?= "0.00000042",
-      testCase "-0.00000042" $
-          show (-0.00000042 :: Decimal) @?= "-0.00000042"
-    ]
+  testGroup "Data.Fixed.Decimal" [
+    testFor (minBound :: Decimal Int 5) maxBound,
+    testFor (minBound :: Decimal Int128 10) maxBound,
+    testFor (minBound :: Decimal Int256 25) maxBound,
+    testFor
+      (fromRational @(Decimal Integer 25) . toRational $ (minBound :: Decimal Int256 25))
+      (fromRational @(Decimal Integer 25) . toRational $ (maxBound :: Decimal Int256 25))
   ]
-
-prop_sientificEq :: Scientific -> Property
-prop_sientificEq s =
-  inRange s ==> s === (toScientific . fromScientific) s
-
-prop_rationalEq :: Scientific -> Property
-prop_rationalEq s =
-    inRange s ==> s === (fromRational . toRational . fromRational @Decimal . toRational) s
-
-prop_divisionMultiplication :: Decimal -> Decimal -> Property
-prop_divisionMultiplication a b =
-    (a * b / a === b) .&&. (a * b / b === a)
-
-prop_decimalPlusEq :: Decimal -> Decimal -> Property
-prop_decimalPlusEq dl dr =
-    toRational (el + er) === toRational (dl + dr) 
-    where
-        l = toRational dl
-        r = toRational dr
-        el = fromRational @DD.Decimal l
-        er = fromRational @DD.Decimal r
-
-prop_precision :: (Positive Int) -> (Positive Int) -> Property
-prop_precision (Positive n) (Positive di) =
-    let d = fromIntegral @_ @Double di / 1000.0
-        de = fromIntegral @_ @Decimal di / 1000
-    in viaSum d /= (fromIntegral n) * d ==> viaSum de === (fromIntegral n) * de 
-    where
-        viaSum d = sum $ replicate n d
-
-
-inRange :: Scientific -> Bool
-inRange s =
-    s >= toScientific minBound &&
-    s <= toScientific maxBound &&
-    base10Exponent s > -lim &&
-    base10Exponent s < lim - 1
-    where
-        lim = scale (undefined :: Decimal)
-
-toScientific :: Decimal -> Scientific
-toScientific d =
-    scientific (fromIntegral . mantissa $ d) (negate . scale $ d)
-
-fromScientific :: Scientific -> Decimal
-fromScientific s =
-    decimal (coefficient s) (base10Exponent s)
-
-
-instance Arbitrary Decimal where
-    arbitrary =
-        D.Decimal . (* (10 ^ ((s `div` 2 + 1)))) <$> chooseBoundedIntegral (-lim, lim)
-        where
-            s = scale @Decimal undefined
-            p = floor @Double @Int . logBase 10 . fromIntegral $ maxBound @(Precision Decimal)
-            lim = maxBound @(Precision Decimal) `mod` (10 ^ (p `div` 3))
-
-instance Arbitrary DD.Decimal where
-    arbitrary = flip suchThat (\d -> let sd = show d in '.' `notElem` sd || last sd /= '0') $ do
-        fromRational . toRational <$> arbitrary @Decimal
 
 
 main :: IO ()
